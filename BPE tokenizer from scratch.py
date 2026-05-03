1c29d1de8f5af14b726d980e3344e00a5381508f
Vraj Patel <vrajp1052005@gmail.com>
2026-05-03 03:26:52 -0400

    completed bpe

--- BPE tokenizer from scratch.py
+++ BPE tokenizer from scratch.py
@@ -46,5 +46,45 @@ def get_pair_counts(sequences):
     return pair_counts
 
 
-pair_ls = get_pair_counts(initial_ls)
 
+# print("Top pair:", top_pair[0], top_pair[1])
+# print(new_ls)
+
+# # output :
+# rojects\Deep Learning Projects\Transfer Learning for nlp>python -u "c:\My Projects\Deep Learning Projects\Transfer Learning for nlp\BPE tokenizer from scratch.py"
+# Top pair: i n
+# [['T', 'h', 'e</w>'], ['v', 'e', 'r', 'y</w>'], ['n', 'a', 'm', 'e</w>'], ['s', 't', 'r', 'i', 'k', 'e', 's</w>'], ['f', 'e', 'a', 'r</w>'], ['a', 'n', 'd</w>'], ['a', 'w', 'e</w>'], ['in', 't', 'o</w>'], ['t', 'h', 'e</w>'], ['h', 'e', 'a', 'r', 't', 's</w>'], ['o', 'f</w>'], ['p', 'r', 'o', 'g', 'r', 'a', 'm', 'm', 'e', 'r', 's</w>'], ['w', 'o', 'r', 'l', 'd', 'w', 'i', 'd', 'e', '.</w>'], ['W', 'e</w>'], ['a', 'l', 'l</w>'], ['k', 'n', 'o', 'w</w>'], ['w', 'e</w>'], ['o', 'u', 'g', 'h', 't</w>'], ['t', 'o</w>'], ['“', 's', 'u', 'p', 'p', 'o', 'r', 't</w>'], ['U', 'n', 'i', 'c', 'o', 'd', 'e', '”</w>'], ['i', 'n</w>'], ['o', 'u', 'r</w>'], ['s', 'o', 'f', 't', 'w', 'a', 'r', 'e</w>'], ['(', 'w', 'h', 'a', 't', 'e', 'v', 'e', 'r</w>'], ['t', 'h', 'a', 't</w>'], ['m', 'e', 'a', 'n', 's', '—', 'l', 'i', 'k', 'e</w>'], ['u', 's', 'in', 'g</w>'], ['w', 'c', 'h', 'a', 'r', '_', 't</w>'], ['f', 'o', 'r</w>'], ['a', 'l', 'l</w>'], ['t', 'h', 'e</w>'], ['s', 't', 'r', 'in', 'g', 's', ',</w>'], ['r', 'i', 'g', 'h', 't', '?', ')', '.</w>'], ['B', 'u', 't</w>'], ['U', 'n', 'i', 'c', 'o', 'd', 'e</w>'], ['c', 'a', 'n</w>'], ['b', 'e</w>'], ['a', 'b', 's', 't', 'r', 'u', 's', 'e', ',</w>'], ['a', 'n', 'd</w>'], ['d', 'i', 'v', 'in', 'g</w>'], ['in', 't', 'o</w>'], ['t', 'h', 'e</w>'], ['t', 'h', 'o', 'u', 's', 'a', 'n', 'd', '-', 'p', 'a', 'g', 'e</w>'], ['U', 'n', 'i', 'c', 'o', 'd', 'e</w>'], ['S', 't', 'a', 'n', 'd', 'a', 'r', 'd</w>'], ['p', 'l', 'u', 's</w>'], ['i', 't', 's</w>'], ['d', 'o', 'z', 'e', 'n', 's</w>'], ['o', 'f</w>'], ['s', 'u', 'p', 'p', 'l', 'e', 'm', 'e', 'n', 't', 'a', 'r', 'y</w>'], ['a', 'n', 'n', 'e', 'x', 'e', 's', ',</w>'], ['r', 'e', 'p', 'o', 'r', 't', 's', ',</w>'], ['a', 'n', 'd</w>'], ['n', 'o', 't', 'e', 's</w>'], ['c', 'a', 'n</w>'], ['b', 'e</w>'], ['m', 'o', 'r', 'e</w>'], ['t', 'h', 'a', 'n</w>'], ['a</w>'], ['l', 'i', 't', 't', 'l', 'e</w>'], ['in', 't', 'i', 'm', 'i', 'd', 'a', 't', 'in', 'g', '.</w>'], ['I</w>'], ['d', 'o', 'n', '’', 't</w>'], ['b', 'l', 'a', 'm', 'e</w>'], ['p', 'r', 'o', 'g', 'r', 'a', 'm', 'm', 'e', 'r', 's</w>'], ['f', 'o', 'r</w>'], ['s', 't', 'i', 'l', 'l</w>'], ['f', 'in', 'd', 'in', 'g</w>'], ['t', 'h', 'e</w>'], ['w', 'h', 'o', 'l', 'e</w>'], ['t', 'h', 'in', 'g</w>'], ['m', 'y', 's', 't', 'e', 'r', 'i', 'o', 'u', 's', ',</w>'], ['e', 'v', 'e', 'n</w>'], ['3', '0</w>'], ['y', 'e', 'a', 'r', 's</w>'], ['a', 'f', 't', 'e', 'r</w>'], ['U', 'n', 'i', 'c', 'o', 'd', 'e', '’', 's</w>'], ['in', 'c', 'e', 'p', 't', 'i', 'o', 'n', '.</w>']]
+
+
+merge_rules = []
+num_merges = 50
+
+for _ in range(num_merges):
+    # 1. get pair counts from current sequences
+    pair_dict = get_pair_counts(initial_ls)
+    # 2. find top pair
+    output = dict(reversed(sorted(pair_dict.items(), key=lambda item: item[1])))
+    top_pair = list(output.keys())[0]
+    # 3. merge it
+    new_ls = []
+    for i in initial_ls:
+        new_word = []
+        idx = 0
+        while idx < len(i):
+            if idx < len(i) - 1 and i[idx] == top_pair[0] and i[idx + 1] == top_pair[1]:
+                new_word.append(top_pair[0] + top_pair[1])   
+                idx += 2
+            else:
+                new_word.append(i[idx])
+                idx += 1
+        new_ls.append(new_word)
+    # 4. update your sequences
+    initial_ls = new_ls
+    # 5. store the top pair in merge_rules
+    merge_rules.append(top_pair)
+
+print(merge_rules)
+
+
+# output  
+[('i', 'n'), ('t', 'h'), ('a', 'n'), ('a', 'r'), ('s', 't'), ('s', ',</w>'), ('in', 'g</w>'), ('o', 'd'), ('c', 'od'), ('i', 'cod'), ('n', 'icod'), ('U', 'nicod'), ('o', 'u'), ('o', 'r'), ('m', 'e'), ('th', 'e</w>'), ('a', 'm'), ('a', 'n</w>'), ('t', 'e'), ('l', 'l</w>'), ('t', 'o</w>'), ('an', 'd</w>'), ('r', 'i'), ('v', 'e'), ('o', 'n'), ('t', 'i'), ('l', 'e</w>'), ('e', 'p'), ('i', 't'), ('p', 'l'), ('an', 'd'), ('b', 'e</w>'), ('c', 'an</w>'), ('Unicod', 'e</w>'), ('in', 'g'), ('st', 'r'), ('o', 'r</w>'), ('f', 'or</w>'), ('u', 's'), ('w', 'h'), ('Unicod', 'e'), ('u', 'p'), ('s', 'up'), ('g', 'h'), ('n', 'o'), ('a', 'll</w>'), ('i', 'd'), ('r', 's</w>'), ('me', 'rs</w>'), ('am', 'mers</w>')]
